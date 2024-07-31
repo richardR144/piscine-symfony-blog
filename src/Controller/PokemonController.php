6f9b8cf0c2656afc2c96607961e4c52b8af12c78
richardR144 <richard.rigollet@lapiscine.pro>
2024-07-31 11:45:32 +0200

    Commentaires

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Pokemon;
+use App\Form\PokemonType;
 use App\Repository\PokemonRepository;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -54,9 +56,9 @@ class PokemonController extends AbstractController
     public function deletePokemons(int $id, PokemonRepository $pokemonRepository, EntityManagerInterface $entityManager): Response
     {
         $pokemon = $pokemonRepository->find($id);
-if (!$pokemon)
-    $html = $this->renderView(view:'page/404.html.twig');
-return new Response($html, status:404);
+        if (!$pokemon)
+            $html = $this->renderView(view: 'page/404.html.twig');
+        return new Response($html, status: 404);
         // j'utilise la classe entity manager
         // pour préparer la requête SQL de suppression
         // cette requête n'est pas executée tout de suite
@@ -67,22 +69,20 @@ return new Response($html, status:404);
         return $this->redirectToRoute('pokemon_list_db');
     }
 
-    #[Route('/pokemons/insert/without-form', name: 'insert_pokemon')]
+    #[Route('/pokemons/insert/form-builder', name: 'insert_pokemon_form_builder')]
     public function insertPokemon(EntityManagerInterface $entityManager, Request $request)
     {
-
         // j'initialise la variable
         // $pokemon à null
         // car on va l'envoyer à twig (et on fera une vérif dans twig)
         $pokemon = null;
 
-
         // je vérifie si la requête est du POST
         // donc si le form a été envoyé
         if ($request->getMethod() === 'POST') {
 
-
             // je récupère les données envoyées par l'utilisateur
+            //handlerequest ci-dessous ça renvoie POST
             $title = $request->request->get('title');
             $description = $request->request->get('description');
             $image = $request->request->get('image');
@@ -94,11 +94,11 @@ return new Response($html, status:404);
             // je passe en valeur des propriétés de la classe
             // pokemon les données envoyées par l'utilisateur
             // grâce aux fonctions setters
+            //juste en dessous Handlerequest
             $pokemon->setTitle($title);
-            $pokemon->setDescription( $description);
+            $pokemon->setDescription($description);
             $pokemon->setImage($image);
             $pokemon->setType($type);
-
 
             // j'enregistre l'instance de la classe
             // pokemon dans la table pokemon
@@ -106,18 +106,61 @@ return new Response($html, status:404);
             $entityManager->persist($pokemon);
             $entityManager->flush();
         }
-
-
-        // je retourne une réponse HTTP
-        // avec le html du formulaire
+        // je retourne une réponse HTTP avec le html du formulaire
         return $this->render('page/pokemon_insert_without_form.html.twig', [
             'pokemon' => $pokemon,
         ]);
-
     }
 
-}
+    #[Route('/pokemons/insert/form-builder', name: 'insert_pokemon_form_builder')]
+    public function insertPokemonFormBuilder(Request $request, EntityManagerInterface $entityManager)
+    {
 
+        //on créé une class de gabarit de formulaire HTML avec php bin/console make: form
+        //ensuite je créé une instance de class d'entité Pokemon
+        $pokemon = new Pokemon();
+
+        //cela permet de générer une instance de la class de gabarit formulaire et de la lier
+        //à l'instance de l'entité
+        $pokemonForm = $this->createForm(PokemonType::class, $pokemon);
+        //je lie le formulaire avec la requête
+        $pokemonForm->handleRequest($request);
+
+        //si le formulaire a été envoyé et que les données sont ok
+
+        if ($pokemonForm->isSubmitted() && $pokemonForm->isValid()) {
+            $entityManager->persist($pokemon);
+            $entityManager->flush();
+        }
+        return $this->render('page/pokemon_insert_form_builder.html.twig', [
+            'pokemonForm => $pokemonForm->createView('
+        ]);
+    }
+
+
+   // j'ai créé une route (integration dans la route de l'id du pokemon a modifier/cibler un élément de ma base de donnée
+    #[Route('/pokemon/update/{id}', name: 'pokemon_update')]
+    public function pokemonUpdate( int $id, pokemonRepository $pokemonRepository, Request $request, EntityManagerInterface $entityManager)
+    {
+        //je sélectionne un pokemon par son id
+        $pokemon = $pokemonRepository->find($id);
+        //génere une instance de pokemon créé à la ligne précédente
+        $pokemonUpdateForm = $this->createForm(PokemonType::class, $pokemon);
+        //lié une requête au formulaire
+        $pokemonUpdateForm->handleRequest($request);
+        //si les conditions sont remplis
+        if($pokemonUpdateForm->isSubmitted() && $pokemonUpdateForm->isValid()) {
+            //je prépare mon update
+            $entityManager->persist($pokemon);
+            //je valide la modification de l'update
+            $entityManager->flush();
+        }
+        //renvoie le résultat
+        return $this->render('page/pokemon_update.html.twig', [
+            'pokemonUpdateForm' => $pokemonUpdateForm->createView()
+        ]);
+    }
+}
 
 
 
